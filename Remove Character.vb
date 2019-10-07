70fb2d482d6690a7615b1fd660d62291b2366c30
hassanshahani <52282652+hassanshahani@users.noreply.github.com>
2019-10-07 21:58:35 +0500

    Remove Character from String.
    This code removes unwanted character from a string. It locates the unwanted character in a string and in discards it.

---
+++ Remove Character.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, str2, nextChar As String
+        Dim Char1 As Char
+        Dim count As Integer
+
+        str1 = ""
+        str2 = ""
+        Char1 = ""
+        nextChar = ""
+        count = 0
+
+
+        Console.Write("Enter the String: ")
+        str1 = Console.ReadLine
+
+        Console.Write("Enter Character to remove: ")
+        Char1 = Console.ReadLine
+
+        For count = 1 To Len(str1)
+            nextChar = Mid(str1, count, 1)
+            If nextChar <> Char1 Then
+                str2 = str2 & nextChar
+            End If
+
+        Next
+
+        Console.WriteLine("Final String is: " & str2)
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
