e4a6722542399c7fca3fa292580313c253430922
hassanshahani <52282652+hassanshahani@users.noreply.github.com>
2019-10-20 21:16:17 +0500

    Alphabets and Digits Counter
    This program count the no. of alphabets and digits in a string and displays results in the end.

---
+++ Alphabets and Digits counter.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1 As String
+        Dim chr As String
+        Dim alpha, digit, count As Integer
+
+        str1 = ""
+        count = 0
+        alpha = 0
+        digit = 0
+        chr = ""
+
+        Console.Write("Enter word : ")
+        str1 = Console.ReadLine
+
+        For count = 1 To Len(str1)
+            chr = Mid(str1, count, 1)
+            If LCase(chr) > "a" And LCase(chr) < "z" Then
+                alpha = alpha + 1
+            ElseIf UCase(chr) > "A" And UCase(chr) < "Z" Then
+                alpha = alpha + 1
+            ElseIf chr >= 0 And chr <= 9 Then
+                digit = digit + 1
+            End If
+        Next
+
+        Console.WriteLine("No. of aplhabets are : " & alpha)
+        Console.WriteLine("No. of digits are : " & digit)
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
