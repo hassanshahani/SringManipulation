c4b625f887694cfa24bb978f56a76e7d381bed6e
hassanshahani <52282652+hassanshahani@users.noreply.github.com>
2019-11-18 21:38:28 +0500

    Monthly Test

--- Module1.vb
+++ Module1.vb
@@ -1,42 +1,45 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim str1, str2, nextChar As String
-        Dim chr1, chr2 As Char
-        Dim count As Integer
+        Dim Bstr As String
+        Dim bcount, count1, count2, sum As Integer
+        Dim validBinaryString As Boolean
 
-        Str1 = ""
-        Str2 = ""
-        chr1 = ""
-        chr2 = ""
-        count = 0
-        nextChar = ""
+        Bstr = ""
+        bcount = 0
+        count1 = 0
+        count2 = 0
+        sum = 0
+        validBinaryString = True
 
+        Console.Write("Enter Binary Number : ")
+        Bstr = Console.ReadLine
 
+        bcount = Len(Bstr)
 
+        If bcount < 1 Or bcount > 8 Then
+            validBinaryString = False
+        Else
+            For count1 = 1 To bcount
+                If Mid(Bstr, count1, 1) = "0" And Mid(Bstr, count1, 1) = "1" Then
+                    validBinaryString = True
+                Else
+                    validBinaryString = False
+                End If
+                Exit For
+            Next
+        End If
 
-        Console.Write("Enter string 1: ")
-        str1 = Console.ReadLine()
-
-        Console.Write("Enter character to replace: ")
-        chr1 = Console.ReadLine
-
-        Console.Write("Enter charatcter to replace with: ")
-        chr2 = Console.ReadLine
-
-        For count = 1 To Len(str1)
-            nextChar = Mid(str1, count, 1)
-
-            If nextChar = chr1 Then
-                str2 = str2 & chr2
-            Else
-                str2 = str2 & nextChar
-
-            End If
-        Next
-        Console.WriteLine(str2)
+        If validBinaryString = False Then
+            Console.WriteLine("Not a valid Binary Number")
+        ElseIf validBinaryString = True Then
+            For count2 = 0 To bcount - 1
+                sum = Mid(Bstr, bcount - count2, 1) * 2 ^ (count2) + sum
+            Next
+            Console.WriteLine("Valid Binary Number")
+            Console.WriteLine("Denary Number = " & sum)
+        End If
         Console.ReadKey()
-
     End Sub
 
 End Module
